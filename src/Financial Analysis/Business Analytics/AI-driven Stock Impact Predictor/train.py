7f8f41cbaf1e96074da84ea2c135a9f174aa14cc
REYYI SHREYAS <89481402+reyyishreyas@users.noreply.github.com>
2026-05-21 00:06:17 +0530

    changed the base ML. method for training
    changed the ml method for training to XGBOOST which slightly helped in decreasing the R2 score as compared to RF forest and LightGBM

--- src/Financial Analysis/Business Analytics/AI-driven Stock Impact Predictor/train.py
+++ src/Financial Analysis/Business Analytics/AI-driven Stock Impact Predictor/train.py
@@ -1,8 +1,8 @@
 # train.py
 import joblib
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error,r2_score
 from preprocess import load_and_preprocess
 
 if __name__ == "__main__":
@@ -13,8 +13,18 @@ if __name__ == "__main__":
 
     # Create model pipeline
     model = Pipeline(steps=[
+
         ('preprocessor', preprocessor),
-        ('regressor', RandomForestRegressor(n_estimators=100, random_state=42))
+
+        ('regressor', XGBRegressor(
+            n_estimators=300,
+            learning_rate=0.05,
+            max_depth=6,
+            subsample=0.8,
+            colsample_bytree=0.8,
+            objective='reg:squarederror',
+            random_state=42
+        ))
     ])
 
     # Train
@@ -26,7 +36,11 @@ if __name__ == "__main__":
     # Evaluate
     mae = mean_absolute_error(y_test, y_pred)
     mse = mean_squared_error(y_test, y_pred)
-    print(f"MAE: {mae:.4f}, MSE: {mse:.4f}")
+    r2 = r2_score(y_test, y_pred)
+
+    print(f"MAE: {mae:.4f}")
+    print(f"MSE: {mse:.4f}")
+    print(f"R2 Score: {r2:.4f}")
 
     # Save model
     joblib.dump(model, "model/trained_model.pkl")
